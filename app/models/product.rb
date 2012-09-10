e4b3dd4ff6810ba70ba9c9068118035086c19817
raghuveertele <raghuveer.g@teleparadigm.com>
2012-09-10 12:53:50 +0530

    Layout Updated...

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
+  default_scope :order => 'title'
   attr_accessible :description, :image_url, :price, :title
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
@@ -7,4 +8,5 @@ class Product < ActiveRecord::Base
     :with => %r{\.(gif|jpg|png)$}i,
     :message => 'must be a URL for GIF, JPG or PNG image.'
   }
+  validates :title, :length => {:minimum => 10}
 end
